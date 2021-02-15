659f634c3007afd8d6243f8b7a673bfa5b526221
Spencer Hubbard <hubbardspencerm@gmail.com>
2021-02-15 13:03:21 -0800

    Add tests for sh-ghc

--- .emacs.d/sh/sh-ghc.el
+++ .emacs.d/sh/sh-ghc.el
@@ -16,15 +16,11 @@
 (require 'seq)
 (require 'flymake)
 
-;; TODO change default from stack to ghc.
-(defvar sh-ghc-command
-  "stack ghc --"
-  "Command for invoking GHC.")
-
 (defun sh-ghc-flymake (report-fn &rest _args)
   "Flymake backend for GHC with REPORT-FN.
 An error is thrown if GHC command cannot be found."
-  (unless (executable-find sh-ghc-command)
+  ;; TODO support commands for invoking GHC other than Stack.
+  (unless (executable-find "stack")
     (error "Cannot find GHC command"))
   ;; Use a temp file with contents of current buffer.  We must be careful with
   ;; how we name the temp file because GHC checks the file extension.
@@ -49,19 +45,20 @@ An error is thrown if GHC command cannot be found."
   "Initialize Flymake backend for GHC."
   (add-hook 'flymake-diagnostic-functions 'sh-ghc-flymake nil t))
 
-;; See https://gitlab.haskell.org/ghc/ghc for details on JSON encoding of
-;; errors.
+;; If Stack is used, then this command could take a while to run the first time
+;; it is invoked, depending on which dependencies are needed.  See
+;; https://gitlab.haskell.org/ghc/ghc for details on JSON encoding of errors.
 (defun sh-ghc--command (file)
   "Invoke GHC on FILE.
 An error is thrown if GHC command cannot be found."
-  (unless (executable-find sh-ghc-command)
+  ;; TODO support commands for invoking GHC other than Stack.
+  (unless (executable-find "stack")
     (error "Cannot find GHC command"))
   (let* ((opts (list "-fdiagnostics-color=never"
                      "-fno-code"
                      "-ddump-json"
                      "-Wall"))
-         (cmd  (concat sh-ghc-command
-                       " "
+         (cmd  (concat "stack ghc -- "
                        (string-join opts " ")
                        " "
                        (shell-quote-argument file))))
@@ -104,6 +101,7 @@ An error is thrown if GHC command cannot be found."
   "Return Flymake diagnostic region of BUFFER for SPAN."
   ;; Check start and end lines and columns are non-null.
   (pcase span
+    (:null                    (error "Null span"))
     ((map ('startLine :null)) (error "Null start line"))
     ((map ('startCol  :null)) (error "Null start column"))
     ((map ('endLine   :null)) (error "Null end line"))
@@ -140,25 +138,70 @@ An error is thrown if the severity field has an unexpected value."
 
 (require 'ert)
 
-;; (ert-deftest sh-ghc--mk-diag-region-test-1 ()
-;;   ;; TODO write documentation string.
-;;   ;; TODO implement.
-;;   (error "Not implemented"))
+(ert-deftest sh-ghc--mk-diag-p-test-1 ()
+  "Test ‘sh-ghc--mk-diag-p’ with message containing expected keys and severity."
+  (should (sh-ghc--mk-diag-p '((span    . ((startLine . :null)
+                                           (startCol  . :null)
+                                           (endLine   . :null)
+                                           (endCol    . :null)))
+                               (doc      . :null)
+                               (severity . "SevError")
+                               (reason   . :null)))))
 
-;; (ert-deftest sh-ghc--mk-diag-type-test-1 ()
-;;   ;; TODO write documentation string.
-;;   ;; TODO implement.
-;;   (error "Not implemented"))
+(ert-deftest sh-ghc--mk-diag-p-test-2 ()
+  "Test ‘sh-ghc--mk-diag-p’ with message missing expected key."
+  (should-not (sh-ghc--mk-diag-p '((span     . :null)
+                                   (doc      . :null)
+                                   (severity . "SevError")
+                                   (reason   . :null)))))
 
-;; (ert-deftest sh-ghc--mk-diag-text-test-1 ()
-;;   ;; TODO write documentation string.
-;;   ;; TODO implement.
-;;   (error "Not implemented"))
+(ert-deftest sh-ghc--mk-diag-p-test-3 ()
+  "Test ‘sh-ghc--mk-diag-p’ with message containing unexpected severity."
+  (should-not (sh-ghc--mk-diag-p '((span     . ((startLine . :null)
+                                                (startCol  . :null)
+                                                (endLine   . :null)
+                                                (endCol    . :null)))
+                                   (doc      . :null)
+                                   (severity . "SevOutput")
+                                   (reason   . :null)))))
 
-;; (ert-deftest sh-ghc--mk-diag-p-test-1 ()
-;;   ;; TODO write documentation string.
-;;   ;; TODO implement.
-;;   (error "Not implemented"))
+;; Create a temporary buffer using ‘with-temp-buffer’ when testing
+;; ‘sh-ghc--mk-diag-region’.  This helps avoid a dependency on (or change to)
+;; the state of the environment.  See ERT manual for more information.
+(ert-deftest sh-ghc--mk-diag-region-test-1 ()
+  "Test ‘sh-ghc--mk-diag-region’ with message containing non-null start and end
+lines and columns."
+  (with-temp-buffer
+    (insert (make-string 3 ?x))
+    (let ((region (sh-ghc--mk-diag-region (current-buffer)
+                                          '((startLine . 1)
+                                            (startCol  . 1)
+                                            (endLine   . 1)
+                                            (endCol    . 3)))))
+      (should (= (car region) 1))
+      (should (= (cdr region) 4)))))
+
+(ert-deftest sh-ghc--mk-diag-type-test-1 ()
+  "Test ‘sh-ghc--mk-diag-type’ with message containing expected severities."
+  (should (equal (sh-ghc--mk-diag-type '((severity . "SevError")))   :error))
+  (should (equal (sh-ghc--mk-diag-type '((severity . "SevWarning"))) :warning)))
+
+(ert-deftest sh-ghc--mk-diag-type-test-2 ()
+  "Test ‘sh-ghc--mk-diag-type’ with message containing unexpected severities."
+  (should-error (sh-ghc--mk-diag-type '((severity . "SevOutput"))))
+  (should-error (sh-ghc--mk-diag-type '((severity . :null)))))
+
+;; Check each line of the return value of ‘sh-ghc--mk-diag-text’ individually.
+(ert-deftest sh-ghc--mk-diag-text-test-1 ()
+  "Test ‘sh-ghc--mk-diag-text’ with message containing text for doc and reason."
+  (let ((text (sh-ghc--mk-diag-text '((doc      . "Defined but not used: ‘xs’")
+                                      (severity . "SevWarning")
+                                      (reason   . "Opt_WarnUnusedMatches")))))
+    (setq text (split-string text "\n"))
+    (should (= (length text) 3))
+    (should (string= (pop text) "Reason: Opt_WarnUnusedMatches"))
+    (should (string= (pop text) "Message:"))
+    (should (string= (pop text) "Defined but not used: ‘xs’"))))
 
 (provide 'sh-ghc)
 ;;; sh-ghc.el ends here
